b05f6813b67b1c2f43e00b818ac4e69ec5baa66c
Nick C <nick@partnersdentalstudio.com>
2025-11-17 12:07:09 -0600

    format sqlfmt

--- sql_query/task_by_tech_eff.sql
+++ sql_query/task_by_tech_eff.sql
@@ -1,13 +1,13 @@
-SELECT cth.CompletedBy,
-       ca.CaseNumber,
-       cth.Task,
-       cth.CompleteDate,
-       ct.Duration
-       --cth.Rejected
-FROM dbo.CaseTasksHistory AS cth
-INNER JOIN dbo.CaseTasks AS ct ON ct.CaseID = cth.CaseID AND ct.Task = cth.Task AND ct.CaseProductID = cth.CaseProductID
-INNER JOIN dbo.Cases as ca ON ca.CaseID = cth.CaseID
-WHERE cth.CompleteDate >= DATEADD(day, -5, GETDATE())
-AND cth.Rejected = 0
---AND ca.CaseNumber = '145523'
-ORDER BY cth.task ASC;
+select cth.completedby, ca.casenumber, cth.task, cth.completedate, ct.duration
+-- cth.Rejected
+from dbo.casetaskshistory as cth
+inner join
+    dbo.casetasks as ct
+    on ct.caseid = cth.caseid
+    and ct.task = cth.task
+    and ct.caseproductid = cth.caseproductid
+inner join dbo.cases as ca on ca.caseid = cth.caseid
+where cth.completedate >= dateadd(day, -5, getdate()) and cth.rejected = 0
+-- AND ca.CaseNumber = '145523'
+order by cth.task asc
+;
